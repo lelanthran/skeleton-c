ba614c0c2b43f8596da67c2c329a03d47bfe7efb
lelanthran <lelanthran@gmail.com>
2019-08-29 23:40:28 +0200

    Completed the example program with copious comments.

--- src/example1_cli.c
+++ src/example1_cli.c
@@ -15,6 +15,16 @@
  *    line.
  * 3. The program will then return the result of that command to the
  *    environment.
+ *
+ * This program provides a basic framework for a command-line driven
+ * program which has intuitive command argument processing and provides a
+ * comprehensive help message for each command.
+ *
+ * Using this framework lets the program handle commands and lets the
+ * caller intersperse arguments to those commands and options to the main
+ * program without the programmer having to do much work. See the help
+ * message string literal in the help message function below for an
+ * example of the usage.
  */
 
 
@@ -23,16 +33,35 @@
  * Functions for parsing the command-line.
  */
 static const char *find_opt (const char **lopts, const char *lname,
-                             const char **sopts, char sname);
+                              const char **sopts, char sname);
 
 static bool process_args (int argc, char **argv,
                           char ***args, char ***lopts, char ***sopts);
 
+/* Utility function for freeing an allocated array of allocated strings.
+ */
+static void string_array_free (char **sarr);
+
 /* *******************************************************************
  * Functions for the help system.
+ * This is a generic template for displaying your help messages. Each
+ * command that your command-line program supports must have an entry here
+ * describing its use.
+ *
+ * Call the 'print_help_msg' function with NULL to print all the help, or
+ * with a specific command to print just the help message for that
+ * command.
  */
 static void print_help_msg (const char *cmd)
 {
+
+   /* First, #define a single formatted string for each command that the
+    * program recognises. Use the examples here for COMMAND_HELP and
+    * COMMAND_SQUARE.
+    *
+    * Later on, below this, you will use these #defines wherever the help
+    * message is needed.
+    */
 #define COMMAND_HELP    \
 "  help <command>",\
 "     Provide the help message for the specified command <command>.",\
@@ -45,6 +74,11 @@ static void print_help_msg (const char *cmd)
 "     printed.",\
 ""
 
+   /* Place each command string together with its help message into a
+    * single array, as show below for the commands "help" and "square".
+    * If you forget to do this the user will not be able to request help
+    * for a specific command.
+    */
    static const struct {
       const char *cmd;
       const char *msg[20];
@@ -53,6 +87,10 @@ static void print_help_msg (const char *cmd)
       { "square",                { COMMAND_SQUARE           }  },
    };
 
+   /* Now write your full help message as shown below. Note this each
+    * line is a string literal with a comma at the end - this means that
+    * each line is a single string in an array of strings.
+    */
    static const char *msg[] = {
 "Example C program: A command-line program to serve as a template for a",
 "command-line driven program.",
@@ -87,6 +125,10 @@ static void print_help_msg (const char *cmd)
 "----------------",
 "GENERAL COMMANDS",
 "----------------",
+   /* This is where you place your #defines for each command. You can
+    * place empty lines before or after each entry to visually offset them
+    * in the output.
+    */
 COMMAND_HELP,
 "",
 "----------------",
@@ -95,6 +137,9 @@ COMMAND_HELP,
 COMMAND_SQUARE,
    };
 
+   /* There is no need to modify anything for the rest of this function.
+    * Output is determined solely by the two arrays above.
+    */
    if (!cmd) {
       for (size_t i=0; i<sizeof msg/sizeof msg[0]; i++) {
          printf ("%s\n", msg[i]);
@@ -118,44 +163,174 @@ COMMAND_SQUARE,
 }
 
 /* *******************************************************************
- * Functions for handling the commands.
+ * Functions for handling the commands. Each command that your program
+ * supports will have an associated function. For example if your program
+ * supports the command 'list', you will create a function called
+ * 'cmd_list' in this section.
+ *
+ * Each command function will take a single parameter of array-of-strings,
+ * the function will then have all of its arguments in that
+ * array-of-strings.
+ *
+ * The function must return an integer that will be returned unchanged to
+ * the operating system. I recommend using EXIT_SUCCESS and EXIT_FAILURE
+ * because they are already defined for success and failure returns.
  */
+typedef int (command_func_t) (const char **args);
 
+int cmd_help (const char **args)
+{
+   // This is the help command. Help is both a command and an option to
+   // the program. This function implements the help command. Processing
+   // the options is performed by the main program, and the main program
+   // will call print_help_msg() with the correct arguments if the user
+   // requested help via an option.
+   print_help_msg (args[1]);
+   return EXIT_SUCCESS;
+}
 
+int cmd_square (const char **args)
+{
+   // This is the example function of how all your command implementations
+   // will look. Note that on error EXIT_FAILURE is returned while on
+   // success EXIT_SUCCESS is returned.
+   int val = 0;
+   if ((sscanf (args[1], "%i", &val))!=1) {
+      fprintf (stderr, "Failed to scan [%s] as an integer.\n", args[1]);
+      return EXIT_FAILURE;
+   }
+   return EXIT_SUCCESS;
+}
 
 /* *******************************************************************
- * Finally, the main function.
+ * Finally, the main function. Note that we have a small handler to free
+ * all the memory that main allocates.
  */
+char **g_args = NULL,
+     **g_lopts = NULL,
+     **g_sopts = NULL;
+
+void cleanup_func (void)
+{
+   string_array_free (g_args);
+   string_array_free (g_lopts);
+   string_array_free (g_sopts);
+}
 
 int main (int argc, char **argv)
 {
-   char **args = NULL,
-        **lopts = NULL,
-        **sopts = NULL;
+   /* All your commands must be stored in this array, along with the
+    * function to call for that command and the minimum and maximum number
+    * of arguments that the command takes.
+    */
+   static const struct command_t {
+      const char *cmd;
+      int (*fptr) (const char **args);
+      size_t min_args;
+      size_t max_args;
+   } cmds[] = {
+      { "help",                  cmd_help,               2, 2     },
+      { "square",                cmd_square,             3, 3     },
+   };
 
-   int val = 12;
+   static const size_t ncmds = sizeof cmds / sizeof cmds[0];
 
-   if (!(process_args (argc, argv, &args, &lopts, &sopts))) {
+   const struct command_t *cmd = NULL;
+
+
+   printf ("Module one version: [%s]\n", MODULE_ONE_VERSION);
+   printf ("Module two version: [%s]\n", MODULE_TWO_VERSION);
+
+   /* Make sure we call the exit handler if anything happens and we are
+    * not able to return cleanly.
+    */
+   atexit (cleanup_func);
+
+
+   /* Before doing anything else you must process all the arguments to
+    * main(). After processing, all the arguments to the command are
+    * stored in g_args, all the long options found are stored in g_lopts
+    * and all the short options found is stored in g_sopts.
+    *
+    * Later on you can examine the options and perform certain actions
+    * based on them. For example, if '--help' or '-h' is supplied as an
+    * option then you must display the help message for the command in
+    * g_args[0], or the full help message if there are no commands
+    * specified.
+    *
+    * program --help          # Provide full help message
+    * program help square     # Provide the help message for command square
+    * program square --help   # Provide the help message for command square
+    */
+   if (!(process_args (argc, argv, &g_args, &g_lopts, &g_sopts))) {
       fprintf (stderr, "Failed to process argument list "
                        "(argument list too long?)");
       return EXIT_FAILURE;
    }
 
-   const char *opt_help = find_opt ((const char **)lopts,   "help",
-                                     (const char **)sopts,  'h');
+   const char *opt_help = find_opt ((const char **)g_lopts, "help",
+                                    (const char **)g_sopts, 'h');
 
    if (opt_help) {
-      print_help_msg (NULL);
+      // If g_args[0] is NULL, the full help message is printed.
+      print_help_msg (g_args ? g_args[0] : NULL);
       return EXIT_SUCCESS;
    }
 
-   printf ("Module one version: [%s]\n", MODULE_ONE_VERSION);
-   printf ("Module two version: [%s]\n", MODULE_TWO_VERSION);
 
-   printf ("Module one result [%i]: %x\n", val, module_one_example (val));
-   printf ("Module two result [%i]: %x\n", val, module_two_example (val));
 
-   return EXIT_SUCCESS;
+
+   // Count the number of arguments there are in the g_args array
+   size_t nargs = 0;
+   for (size_t i=0; g_args && g_args[i]; i++)
+      nargs++;
+
+   if (!g_args || nargs < 1) {
+      fprintf (stderr, "No command specified. Try --help\n");
+      return EXIT_FAILURE;
+   }
+
+   /* At this point, after you have examined/searched for all the options
+    * you are interested in, the execution of the command proceeds. This
+    * loop simply searches for the correct command. After the loop we
+    * check that the number of arguments are more than the minimum
+    * required and less than the maximum allowed, then runs the command
+    * function.
+    */
+   for (size_t i=0; i<sizeof cmds/sizeof cmds[0]; i++) {
+      if ((strcmp (cmds[i].cmd, g_args[0]))==0) {
+         cmd = &cmds[i];
+         break;
+      }
+   }
+
+
+   // If we did not find an entry in the array, tell the user that the
+   // command is unrecognised and then exit.
+   if (!cmd) {
+      fprintf (stderr, "Unrecognised command: [%s]\n", g_args[0]);
+      return EXIT_FAILURE;
+   }
+
+   // If the user supplied too few or too many arguements, tell them so
+   // and then exit.
+   if (nargs < cmd->min_args) {
+      fprintf (stderr, "[%s] Too few arguments: minimum is %zu, got %zu\n",
+                       cmd->cmd, cmd->min_args, nargs);
+      print_help_msg (cmd->cmd);
+      return EXIT_FAILURE;
+   }
+
+   if (nargs > cmd->max_args) {
+      fprintf (stderr, "[%s] Too many arguments: maximum is %zu, got %zu\n",
+                       cmd->cmd, cmd->max_args, nargs);
+      print_help_msg (cmd->cmd);
+      return EXIT_FAILURE;
+   }
+
+   // Finally, we run the command and return the result to the operating
+   // system.
+   return cmd->fptr ((const char **)g_args);
 }
 
 
